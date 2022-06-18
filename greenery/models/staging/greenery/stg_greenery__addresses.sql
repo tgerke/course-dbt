83deb93ef3c7e533723d8b3ae3b447532d533bf5
Travis Gerke <tgerke@mail.harvard.edu>
2022-06-18 16:06:33 +0000

    add addresses and events models as tables

--- greenery/models/staging/greenery/stg_greenery__addresses.sql
+++ greenery/models/staging/greenery/stg_greenery__addresses.sql
@@ -1,3 +1,9 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
 SELECT 
     address_id,
     address,
--- greenery/models/staging/greenery/stg_greenery__events.sql
+++ greenery/models/staging/greenery/stg_greenery__events.sql
@@ -1,3 +1,9 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
 SELECT 
     event_id,
     session_id,
